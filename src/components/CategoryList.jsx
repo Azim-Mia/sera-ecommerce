0ad13d1b0a5d02781be3651f3231a68bd3bde7fc
azim-mia <mdazimmia53@gmail.com>
2025-01-09 12:42:29 +0600

    change

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -3,42 +3,42 @@ import Image from 'next/image';
 const CategoryList = ()=>{
   return (<div className ="px-4 overflow-x-scroll scrollbar-hide">
 <div className="flex gap-4 md:gap-8">
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
 </Link>
-<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6">
+<Link href="/list?cart=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 2xl:w-1/6 text-center">
 <div className="relative bg-slate-100 w-full h-96">
 <Image src="/images/product_1.jpeg" alt="product" fill sizes="20vw" className="object-cover"/></div>
 <h1 className="mt-8 font-coral text-cl tracking-wide">Category Name</h1>
