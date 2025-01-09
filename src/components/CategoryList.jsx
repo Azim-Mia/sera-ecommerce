9ba4aa9de880228a3ecdc39240768949eaa5520e
azim-mia <mdazimmia53@gmail.com>
2025-01-09 10:30:34 +0600

    modify

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,77 +1,51 @@
 import Link from 'next/link';
 import Image from 'next/image';
-const Footer =()=>{
-  return(<>
-  <div className="flex justify-center gap-24 items-center py-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-[#000] text-[#FFF] text-sm mt-24 xs:flex-col sm:flex-row">
-    {/*TOP*/}
-  <div className="flex justify-center text-center flex-col xs:flex-col md:flex-row sm:flex-row justify-between gap-x-24 gap-y-10">
-    {/*LEFT*/}
-    <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-4">
-    <Link href="/"><div className="text-2xl tracking-wide"> LIMA</div></Link>
-    <p className="text-semibold text-1xl">Dewangonj Market, Fassion_House</p>
-  <span className="text-semibold">mdazimmia53@gmail.com</span>
-  <span className="text-semibold">+8801703357281</span>
-  <div className="flex gap-6">
-<Image src="/facebook.png" width={28} height={28} alt="image" />
-<Image src="/instagram.png" width={28} height={28} alt="image" />
-<Image src="/youtube.webp" width={28} height={28} alt="image" />
-<Image src="/twitter.webp" width={28} height={28} alt="image" />
+const CategoryList = ()=>{
+  return (<div className = "mt-10 mb-2 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+  <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+  <div className="relative h-96 w-full">
+    <Image src="/product_2.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
   </div>
-    </div>
-    {/*CENTER*/}
-        <div className="flex gap-4 xs:ml-12">
-        <div className="flex flex-col gap-6">
-        <h1>COMPANY</h1>
-        <div className="flex flex-col">
-        <Link href="/">About</Link>
-       <Link href="/">About</Link>
-      <Link href="/">About</Link>
-     <Link href="/">About</Link>  
-        </div>
-        </div>
-      <div className="flex flex-col gap-6">
-        <h1>SHOPE</h1>
-        <div className="flex flex-col">
-        <Link href="/">About</Link>
-       <Link href="/">About</Link>
-      <Link href="/">About</Link>
-     <Link href="/">About</Link>  
-        </div>
-        </div>
-       <div className="flex flex-col gap-6">
-        <h1>HELP</h1>
-        <div className="flex flex-col">
-        <Link href="/">About</Link>
-       <Link href="/">About</Link>
-      <Link href="/">About</Link>
-     <Link href="/">About</Link>  
-        </div>
-        </div>
-        </div>
-    {/*RIGHT*/}
- <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-4">
-      <div className="">
-      <h1 className="font-medium">SUBSCRIBE</h1>
-      <p className="mt-4">Be the firsr to get the latest news aboud trends, promotions, and much more! </p>
-      </div>
-      <div className="flex">
-    <input type="text" name="" placeholder="email address" className="p-4 text-[#000] text-center w-3/4 text-2xl " />
-      <button className="w-1/4 bg-lama text-white rounded-sm text-2xl">JOIN</button>
-      </div>
-      <div>
-      <span className="text-semibold">Surce Payments</span>
-        <div className="flex justify-between" >
-      <Image src="/instagram.png" width={40} height={20} alt="image" />
-      <Image src="/instagram.png" width={40} height={20} alt="image" />
-      <Image src="/instagram.png" width={40} height={20} alt="image" />
-      <Image src="/instagram.png" width={40} height={20} alt="image" />
-      <Image src="/instagram.png" width={40} height={20} alt="image" />
-      </div>
-      </div>
-      </div>
+  <div className="flex justify-between">
+  <span className="font-medum">Product Name</span>
+    <span className="font-semibold">Price: $**</span>
   </div>
+  <div className="text-sm text-gray">describtion</div>
+  <button className="rounded-2xl ring-1 ring-lama text-lame py-2 px-4 hover:bg-lama hover:text-white">Add-to-cart</button>
+  </Link>
+   <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+  <div className="relative h-96 w-full">
+    <Image src="/product_5.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
   </div>
-  <div className="bg-[#000] text-white">buttom</div>
-  </>)
+  <div className="flex justify-between">
+  <span className="font-medum">Product Name</span>
+    <span className="font-semibold">Price: $**</span>
+  </div>
+  <div className="text-sm text-gray">describtion</div>
+  <button className="rounded-2xl ring-1 ring-lama text-lame py-2 px-4 hover:bg-lama hover:text-white">Add-to-cart</button>
+  </Link>
+     <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+  <div className="relative h-96 w-full">
+    <Image src="/product_6.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
+  </div>
+  <div className="flex justify-between">
+  <span className="font-medum">Product Name</span>
+    <span className="font-semibold">Price: $**</span>
+  </div>
+  <div className="text-sm text-gray">describtion</div>
+  <button className="rounded-2xl ring-1 ring-lama text-lame py-2 px-4 hover:bg-lama hover:text-white">Add-to-cart</button>
+  </Link>
+     <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
+  <div className="relative h-96 w-full">
+    <Image src="/product_7.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
+  </div>
+  <div className="flex justify-between">
+  <span className="font-medum">Product Name</span>
+    <span className="font-semibold">Price: $**</span>
+  </div>
+  <div className="text-sm text-gray">describtion</div>
+  <button className="rounded-2xl ring-1 ring-lama text-lame py-2 px-4 hover:bg-lama hover:text-white">Add-to-cart</button>
+  </Link>
+  </div>)
 }
-export default Footer;
+export default CategoryList;
