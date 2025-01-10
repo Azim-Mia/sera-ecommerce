83096cd9020169828afcf36dcf9f8f2488484bd9
azim-mia <mdazimmia53@gmail.com>
2025-01-10 11:10:30 +0600

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
+    remotePatterns:[
+      {
+      protocol:"https",
+      hostname:"google.com",
+      },
+    ]
+  },
+    images:{
     localPatterns:[
       {
 pathname: '/images/**',
         search: '',
       },
     ]
-  }
+    }
 };
 
 export default nextConfig;
