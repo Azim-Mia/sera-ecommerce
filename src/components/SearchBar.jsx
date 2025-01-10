ef1f95f5b6efd2307b831783e240d4049cd03d58
azim-mia <mdazimmia53@gmail.com>
2025-01-10 11:20:38 +0600

    change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 'use client';
 import React,{useState, useRef} from 'react';
 import Image from 'next/image';
+import Search from '../../public/images/search.png'
 import {useRouter} from 'next/navigation';
 const SearchBar = ()=>{
   const searchValue = useRef();
@@ -15,7 +16,7 @@ const SearchBar = ()=>{
   return (<>
   <form onSubmit={handleSearch} className="flex gap-3 text-2xl h-10 mt-3 mb-2">
   <input type="text" ref={searchValue} name="search" placeholder="search products" className=" border text-center rounded-md cursor-pointer w-3/4 sm:w-5/6" />
-    <button type="submit"><Image src ="/images/search.png" alt="logo" width={32} height={32} className="h-10 rounded-md" /></button>
+    <button type="submit"><Image src ={Search} alt="logo" width={32} height={32} className="h-10 rounded-md" /></button>
     </form>
   </>)
 }
