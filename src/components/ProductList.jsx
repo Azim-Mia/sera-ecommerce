908aa599c91b7283e7eb770b5ad350e3c94fddee
azim-mia <mdazimmia53@gmail.com>
2025-01-10 21:11:00 +0600

    modify

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,15 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import product_1 from '../assets/images/product_1.jpeg'
+import product_2 from '../assets/images/product_2.jpeg'
+import product_3 from '../assets/images/product_3.jpeg'
+import product_4 from '../assets/images/product_4.jpeg'
+import product_5 from '../assets/images/product_5.jpeg'
 const ProductLists = ()=>{
   return (<div className = "mt-10 mb-2 flex gap-x-8 gap-y-16 justify-between flex-wrap">
   <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
   <div className="relative h-96 w-full">
-    <Image src="/images/product_2.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
+    <Image src={product_1} alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
   </div>
   <div className="flex justify-between">
   <span className="font-medum">Product Name</span>
@@ -15,7 +20,7 @@ const ProductLists = ()=>{
   </Link>
    <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
   <div className="relative h-96 w-full">
-    <Image src="/images/product_5.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
+    <Image src={product_2} alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
   </div>
   <div className="flex justify-between">
   <span className="font-medum">Product Name</span>
@@ -26,7 +31,7 @@ const ProductLists = ()=>{
   </Link>
      <Link href="/list" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
   <div className="relative h-96 w-full">
-    <Image src="/images/product_6.jpeg" alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
+    <Image src={product_3}  alt="product" fill size="25vw" className="absolute object-cover rounded-sm hover:opacity-0 transition-opacity  easy duration-500" />
   </div>
   <div className="flex justify-between">
   <span className="font-medum">Product Name</span>
